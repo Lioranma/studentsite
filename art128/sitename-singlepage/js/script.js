7310c3029197d692c8598dd161331333e7ddac8e
Lioranma <athlan@hawaii.edu>
2021-08-03 21:40:46 -1000

    sitename singlepage completed

--- art128/sitename-singlepage/js/script.js
+++ art128/sitename-singlepage/js/script.js
@@ -1,16 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-    //TOGGLE NAV MOBILE FOR SMALL SCREENS
-    const menubutton = document.querySelector('.menu-button');
-    const menunav = document.querySelector('.toggle-nav');
-    menubutton.addEventListener('click', function() {
-        if (menunav.getAttribute('data-navstate')=== 'open')  {
-            // if true do this
-            menunav.setAttribute('data-navstate' , 'closed');
-        } else {
-            // else (if faulse) do this:
-            menunav.setAttribute('data-navstate' , 'open')
+    //TOGGLE MENU NAV 
+    var menubutton = document.querySelectorAll(".menu-button");
+    var i;
+    for (i = 0; i < menubutton.length; i++) {
+        menubutton[i].onclick = function () {
+            this.classList.toggle("expanded");
         };
-    });
+    }
 
+     //STICKY  NAV SCROLL LINKS FOR SINGLE PAGE SITES 
+     var stickynavlinks = document.querySelectorAll(".sticky nav a");
+     var j;
+     for (i = 0; j < stickynavlinks.length; j++) {
+        stickynavlinks[j].onclick = function () {       
+    var stickymenu = document.querySelectorAll(".sticky .menu-button");
+    var k;
+    for (k = 0; i < stickymenu.length; k++) {
+        stickymenu[k].remove("expanded");
+        };
+    }
+ };
+    
 });
